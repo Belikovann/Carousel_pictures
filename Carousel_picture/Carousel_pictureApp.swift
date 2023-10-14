e32cac694ee2790bdf0b4a91ceb7f21ab10f05b8
@Annabel_ <hydrant94@gmail.com>
2023-10-14 11:58:26 +0200

    Initial Commit

---
+++ Carousel_picture/Carousel_pictureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_pictureApp.swift
+//  Carousel_picture
+//
+//  Created by Аня Беликова on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_pictureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel_picture/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Carousel_picture
+//
+//  Created by Аня Беликова on 14.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
